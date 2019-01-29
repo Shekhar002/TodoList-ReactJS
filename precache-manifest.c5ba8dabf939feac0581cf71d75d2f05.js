bbe1e1208eacff9bee22cf33964380d2a0fe8f9c
Shekhar Tyagi <31857262+ShAvEdPaCk@users.noreply.github.com>
2019-01-29 19:39:55 +0530

    Updates

---
+++ precache-manifest.c5ba8dabf939feac0581cf71d75d2f05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "728647ee8ee96875afc5",
+    "url": "/TodoList-ReactJS/static/css/main.6176fb83.chunk.css"
+  },
+  {
+    "revision": "728647ee8ee96875afc5",
+    "url": "/TodoList-ReactJS/static/js/main.728647ee.chunk.js"
+  },
+  {
+    "revision": "a15771e100b3e20cfcb5",
+    "url": "/TodoList-ReactJS/static/js/1.a15771e1.chunk.js"
+  },
+  {
+    "revision": "138d0cda454cd2702d3c",
+    "url": "/TodoList-ReactJS/static/js/runtime~main.138d0cda.js"
+  },
+  {
+    "revision": "13e8c4cfef263269770577255855da2a",
+    "url": "/TodoList-ReactJS/index.html"
+  }
+];
